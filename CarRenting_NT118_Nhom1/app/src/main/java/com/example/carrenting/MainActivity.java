c70d219cc828d39ff498efec5266aafec33e4f42
Trang Kỳ Anh <20521086@gm.uit.edu.vn>
2022-11-12 12:35:53 +0700

    navigation

--- CarRenting_NT118_Nhom1/app/src/main/java/com/example/carrenting/MainActivity.java
+++ CarRenting_NT118_Nhom1/app/src/main/java/com/example/carrenting/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.carrenting;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ActivityKt;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
