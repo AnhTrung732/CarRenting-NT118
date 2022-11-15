7f6643ce2864751835bf64c155837983682025ce
AnhTrung732 <20520332@gm.uit.edu.vn>
2022-11-15 23:29:32 +0700

    First Page

---
+++ app/src/main/java/com/example/carrenting/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.carrenting;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ActivityKt;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.os.Bundle;
+import android.view.Window;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+
+        setContentView(R.layout.activity_start_app);
+    }
+}
