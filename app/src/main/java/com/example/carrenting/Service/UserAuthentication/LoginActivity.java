758b05da2955956fcd2a1ec2842833695815e2a1
AnhTrung732 <20520332@gm.uit.edu.vn>
2022-11-21 23:46:48 +0700

    Add loginn authentication

---
+++ app/src/main/java/com/example/carrenting/Service/UserAuthentication/LoginActivity.java
@@ -0,0 +1,73 @@
+package com.example.carrenting.Service.UserAuthentication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.carrenting.ActivityPages.StartAppActivity;
+import com.example.carrenting.MainActivity;
+import com.example.carrenting.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText edtTxt_email, edtTxt_password;
+    private Button btn_signIn;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        mAuth = FirebaseAuth.getInstance();
+        edtTxt_email = findViewById(R.id.edtTxt_email);
+        edtTxt_password = findViewById(R.id.edtText_password);
+        btn_signIn = findViewById(R.id.btn_signIn);
+
+        btn_signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        String email, password;
+        email = edtTxt_email.getText().toString();
+        password = edtTxt_password.getText().toString();
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Vui lòng nhập email !!!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Vui lòng nhập mật khẩu", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "Đăng nhập thành công", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Đăng nhập không thành công", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+
+}
