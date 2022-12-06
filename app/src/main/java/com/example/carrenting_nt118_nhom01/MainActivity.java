7b028e02c986b0ee49f1574071077bd522c7bfd5
Trang Kỳ Anh <20521086@gm.uit.edu.vn>
2022-12-07 00:01:07 +0700

    up

--- app/src/main/java/com/example/carrenting_nt118_nhom01/MainActivity.java
+++ app/src/main/java/com/example/carrenting_nt118_nhom01/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.carrenting_nt118_nhom01.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setBackground(null);
+        
     }
 }
