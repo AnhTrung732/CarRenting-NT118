62d6381986c4a01c2edaf4ea333ae25a67e006f5
Trang Kỳ Anh <20521086@gm.uit.edu.vn>
2022-12-07 00:49:42 +0700

    navigation

--- app/src/main/java/com/example/carrenting_nt118_nhom01/MainActivity.java
+++ app/src/main/java/com/example/carrenting_nt118_nhom01/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.carrenting_nt118_nhom01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -15,7 +18,36 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        replaceFragment(new HomeFragment());
         binding.bottomNavigationView.setBackground(null);
-        
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId())
+            {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.activity:
+                    replaceFragment(new ActivityFragment());
+                    break;
+                case R.id.user:
+                    replaceFragment(new UserFragment());
+                    break;
+                case R.id.message:
+                    replaceFragment(new MessageFragment());
+                    break;
+                case R.id.setting:
+                    replaceFragment(new SettingFragment());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
