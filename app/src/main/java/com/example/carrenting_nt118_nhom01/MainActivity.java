76a10fd36d4a7e2b66652c0724e29cad0631ecff
Trang Kỳ Anh <20521086@gm.uit.edu.vn>
2022-12-06 23:15:02 +0700

    up

---
+++ app/src/main/java/com/example/carrenting_nt118_nhom01/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.carrenting_nt118_nhom01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
