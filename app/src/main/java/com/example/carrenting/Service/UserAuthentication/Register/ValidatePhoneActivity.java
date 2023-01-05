b1cf83da87d152124577c98dcc2075f46bedda26
AnhTrung732 <93305749+AnhTrung732@users.noreply.github.com>
2023-01-05 14:24:59 +0700

    Update Register

--- app/src/main/java/com/example/carrenting/Service/UserAuthentication/Register/ValidatePhoneActivity.java
+++ app/src/main/java/com/example/carrenting/Service/UserAuthentication/Register/ValidatePhoneActivity.java
@@ -1,22 +1,27 @@
 package com.example.carrenting.Service.UserAuthentication.Register;
 
+import static androidx.fragment.app.FragmentManager.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.carrenting.ActivityPages.MainActivity;
 import com.example.carrenting.R;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
 
 import java.util.concurrent.TimeUnit;
@@ -26,16 +31,18 @@ public class ValidatePhoneActivity extends AppCompatActivity {
     private EditText otpNumberOne, getOtpNumberTwo, getOtpNumberThree, getOtpNumberFour, getOtpNumberFive, otpNumberSix;
     private Button btnSendCode;
     private TextView tvResend;
-    String verificationId;
+
+    private String mVerificationId;
+    PhoneAuthProvider.ForceResendingToken mResendToken;
+
     String phoneNumber;
     Boolean otpValid = true;
 
-
+    private FirebaseAuth mAuth;
 
     FirebaseAuth firebaseAuth;
     PhoneAuthCredential phoneAuthCredential;
-    PhoneAuthProvider.ForceResendingToken token;
-    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+
 
 
     @Override
@@ -48,6 +55,7 @@ public class ValidatePhoneActivity extends AppCompatActivity {
         phoneNumber = data.getStringExtra("phone");
 
         firebaseAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
         phoneNumber = "+84" + phoneNumber;
         Toast.makeText(this,phoneNumber,Toast.LENGTH_LONG).show();
@@ -66,40 +74,16 @@ public class ValidatePhoneActivity extends AppCompatActivity {
                     String otp = otpNumberOne.getText().toString() + getOtpNumberTwo.getText().toString() + getOtpNumberThree.getText().toString() + getOtpNumberFour.getText().toString() +
                             getOtpNumberFive.getText().toString() + otpNumberSix.getText().toString();
 
-                    PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, otp);
+                    PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, otp);
 
                     verifyAuthentication(credential);
+
+                    Intent intent = new Intent(ValidatePhoneActivity.this, MainActivity.class);
+                    startActivity(intent);
                 }
             }
         });
-        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
 
-            @Override
-            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-                super.onCodeSent(s, forceResendingToken);
-
-                verificationId = s;
-                token = forceResendingToken;
-                tvResend.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onCodeAutoRetrievalTimeOut(@NonNull String s) {
-                super.onCodeAutoRetrievalTimeOut(s);
-                tvResend.setVisibility(View.VISIBLE);
-            }
-
-            @Override
-            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
-                verifyAuthentication(phoneAuthCredential);
-                tvResend.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onVerificationFailed(@NonNull FirebaseException e) {
-                Toast.makeText(ValidatePhoneActivity.this, "OTP Verification Failed.", Toast.LENGTH_SHORT).show();
-            }
-        };
 
         SendOtpCode(phoneNumber);
 
@@ -112,13 +96,48 @@ public class ValidatePhoneActivity extends AppCompatActivity {
         });
 
     }
+    private void VerifyPhoneNumber(String phoneNumber){
+        PhoneAuthOptions options = PhoneAuthOptions.newBuilder(mAuth)
+                .setPhoneNumber(phoneNumber)
+                .setTimeout(60L, TimeUnit.SECONDS)
+                .setActivity(this)
+                .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                        //verifyAuthentication(phoneAuthCredential);
+                        tvResend.setVisibility(View.GONE);
 
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+                        Toast.makeText(ValidatePhoneActivity.this, "OTP Verification Failed.", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        super.onCodeSent(verificationId, forceResendingToken);
+                        Log.d(TAG, "onCodeSent:" + forceResendingToken);
+                        mVerificationId = verificationId;
+                        mResendToken = forceResendingToken;
+                        tvResend.setVisibility(View.GONE);
+
+                    }
+                    @Override
+                    public void onCodeAutoRetrievalTimeOut(@NonNull String s) {
+                        super.onCodeAutoRetrievalTimeOut(s);
+                        tvResend.setVisibility(View.VISIBLE);
+                    }
+                })
+                .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
     private void ReSendOtpCode(String phoneNumber) {
-        PhoneAuthProvider.getInstance().verifyPhoneNumber(phoneNumber,6, TimeUnit.SECONDS,this,mCallbacks);
+        VerifyPhoneNumber(phoneNumber);
     }
 
     private void SendOtpCode(String phoneNumber) {
-        PhoneAuthProvider.getInstance().verifyPhoneNumber(phoneNumber,6, TimeUnit.SECONDS,this,mCallbacks,token);
+        VerifyPhoneNumber(phoneNumber);
     }
     public void verifyAuthentication(PhoneAuthCredential credential){
         firebaseAuth.getCurrentUser().linkWithCredential(credential).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
