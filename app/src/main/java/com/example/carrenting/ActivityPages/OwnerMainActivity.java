c2d781c156fdfac63f6a3ba9b3c09f3dc047eea2
Trang Kỳ Anh <20521086@gm.uit.edu.vn>
2023-01-07 19:38:56 +0700

    update

--- app/src/main/java/com/example/carrenting/ActivityPages/OwnerMainActivity.java
+++ app/src/main/java/com/example/carrenting/ActivityPages/OwnerMainActivity.java
@@ -1,9 +1,27 @@
 package com.example.carrenting.ActivityPages;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.carrenting.FragmentPages.Customer.Customer_ActivityFragment;
+import com.example.carrenting.FragmentPages.Customer.Customer_HomeFragment;
+import com.example.carrenting.FragmentPages.Customer.Customer_MessageFragment;
+import com.example.carrenting.FragmentPages.Customer.Customer_SettingFragment;
+import com.example.carrenting.FragmentPages.Owner.Owner_ActivityFragment;
+import com.example.carrenting.FragmentPages.Owner.Owner_BankingFragment;
+import com.example.carrenting.FragmentPages.Owner.Owner_MessageFragment;
+import com.example.carrenting.FragmentPages.Owner.Owner_SettingFragment;
+import com.example.carrenting.FragmentPages.Owner.Owner_VehicleFragment;
+import com.example.carrenting.R;
+import com.example.carrenting.databinding.ActivityMainBinding;
 
 public class OwnerMainActivity extends AppCompatActivity {
-    /*ActivityMainBinding binding;
+    ActivityMainBinding binding;
     @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +35,13 @@ public class OwnerMainActivity extends AppCompatActivity {
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId())
             {
-                case R.id.home:
+                case R.id.vehicle:
                     replaceFragment(new Owner_VehicleFragment());
                     break;
                 case R.id.activity:
                     replaceFragment(new Owner_ActivityFragment());
                     break;
-                case R.id.vehicle:
+                case R.id.banking:
                     replaceFragment(new Owner_BankingFragment());
                     break;
                 case R.id.message:
@@ -35,11 +53,12 @@ public class OwnerMainActivity extends AppCompatActivity {
             return true;
         });
     }
+
     private void replaceFragment(Fragment fragment)
     {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout_owner, fragment);
         fragmentTransaction.commit();
-    }*/
+    }
 }
